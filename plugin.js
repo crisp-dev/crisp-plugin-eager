49b3fc5c6641b1c2b0134b40b8fce60619dc7f9e
Baptiste Jamin <baptistejamin@gmail.com>
2015-12-12 20:04:12 +0100

    First commit

---
+++ plugin.js
@@ -0,0 +1,16 @@
+(function(){
+
+	var options = INSTALL_OPTIONS;
+
+	if (!options.websiteId) {
+		return;
+	}
+
+	CRISP_WEBSITE_ID = options.websiteId;
+	d=document;
+	s=d.createElement("script");
+	s.src="https://client.crisp.im/l.js";
+	s.async=1;
+	d.getElementsByTagName("head")[0].appendChild(s);
+
+})();
